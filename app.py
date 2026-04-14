706c5910403ac25cef04307c8bdd7802436a1701
shivraj <shivpatil6996@gmail.com>
2026-04-14 16:32:39 +0530

    Birth weight prediction

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,request,jsonify
+import pandas as pd
+import pickle
+app=Flask(__name__)
+@app.route("/predict",methods=["post"])
+
+def  get_prediction():
+    baby_data=request.get_json()
+    baby_df=pd.DataFrame(baby_data)
+    with open("model/model.pkl","rb") as obj:
+        model=pickle.load(obj)
+
+    prediction=model.predict(baby_df)
+    prediction=round(float(prediction[0]),2)
+    response={"prediction":prediction}
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
